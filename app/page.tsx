e0e352e8495c1bb82e0466f689ab648738218e01
Ivan Caceres <ivanny109@gmail.com>
2023-04-19 15:32:53 -0400

    load atm assets sequentially

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,12 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import TerminalScreen from '@/components/TerminalScreen/TerminalScreen'
+import ATM from '@/components/ATM/ATM'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="relative flex place-items-center after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <TerminalScreen />
-        <Image
-          className="relative"
-          src="/bg.webp"
-          alt="Imperial Crest"
-          unoptimized
-          width={1024}
-          height={1024}
-          priority
-        />
-      </div>
+      <ATM />
     </main>
   )
 }
---
+++ components/ATM/ATM.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { FC, useState } from "react"
+import Image from 'next/image'
+import TerminalScreen from "../TerminalScreen/TerminalScreen"
+
+const ATM : FC = () => {
+    const [loaded, setLoaded] = useState(false)
+    console.log('loaded', loaded)
+    return (
+        <div className="relative flex place-items-center">
+            <Image
+            className="relative"
+            src="/bg.webp"
+            alt="Imperial Crest"
+            unoptimized
+            width={1024}
+            height={1024}
+            priority
+            onLoad={() => {setLoaded(true)}}
+            />
+            {loaded && <TerminalScreen />}
+      </div>
+    )
+}
+
+export default ATM
